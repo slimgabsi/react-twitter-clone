cbf88cd0572e130bd90a78c24ae3c7f3226e23fd
SlimGabsi <slimgabsi287@gmail.com>
2022-01-19 14:45:26 +0100

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,41 @@
+import React, { Component, Fragment } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import { handleInitialData } from "../actions/shared";
+import Dashboard from "./Dashboard";
+import LoadingBar from "react-redux-loading";
+import NewTweet from "./NewTweet";
+import TweetPage from "./TweetPage";
+import Nav from "./Nav";
+class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData());
+  }
+  render() {
+    return (
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Fragment>
+          <LoadingBar />
+          <div className="container">
+            <Nav />
+            {this.props.loading === true ? null : (
+              <div>
+                <Route path="/" exact component={Dashboard} />
+                <Route path="/tweet/:id" component={TweetPage} />
+                <Route path="/new" component={NewTweet} />
+              </div>
+            )}
+          </div>
+        </Fragment>
+      </BrowserRouter>
+    );
+  }
+}
+
+function mapStateToProps({ authedUser }) {
+  return {
+    loading: authedUser === null,
+  };
+}
+
+export default connect(mapStateToProps)(App);
---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+import { combineReducers } from "redux";
+import authedUser from "./autheduser";
+import users from "./users";
+import tweets from "./tweets";
+import { loadingBarReducer } from "react-redux-loading";
+
+export default combineReducers({
+  authedUser,
+  users,
+  tweets,
+  loadingBar: loadingBarReducer,
+});
